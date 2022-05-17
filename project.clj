ebd6b649bdfe93494f0a350ee697a325d293592e
spietras <szymon.pietras@magnet.coop>
2022-05-17 10:28:00 +0100

    Bump lein, cljfmt and eastwood dependencies

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/gethop-dev/scheduling.twarc"
   :license {:name "Mozilla Public Licence 2.0"
             :url "https://www.mozilla.org/en-US/MPL/2.0/"}
-  :min-lein-version "2.9.0"
+  :min-lein-version "2.9.8"
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [diehard "0.10.2"]
                  [duct/core "0.8.0"]
@@ -26,5 +26,5 @@
    :repl {:repl-options {:host "0.0.0.0"
                          :port 4001}}
    :profiles/dev {}
-   :project/dev {:plugins [[jonase/eastwood "0.3.11"]
-                           [lein-cljfmt "0.6.7"]]}})
+   :project/dev {:plugins [[jonase/eastwood "1.2.3"]
+                           [lein-cljfmt "0.8.0"]]}})
